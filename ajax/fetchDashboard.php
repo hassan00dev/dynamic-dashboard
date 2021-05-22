12db647055fc6c279cfc6a00ee493446538e28e0
codingwithhassan <codingwithme.hassan@gmail.com>
2021-05-22 11:41:26 -0700

    fetch dashboard with vertical cols position

--- ajax/fetchDashboard.php
+++ ajax/fetchDashboard.php
@@ -17,13 +17,22 @@ function fetch_rows() {
     
     while($row = mysqli_fetch_assoc($query)){
         $row_position = $row['row_position'];
-        $q = mysqli_query($conn,"SELECT * FROM columns WHERE dashboard_id = '$dashboard_id' AND row_position = '$row_position';");
+        $q = mysqli_query($conn,"SELECT * FROM columns WHERE dashboard_id = '$dashboard_id' AND row_position = '$row_position' group by col_position;");
         $index = 0;
         while($col = mysqli_fetch_assoc($q)){
             $responseData[$row_position]['pattern'] = explode(',',$row['pattern']);
             $responseData[$row_position]['cols'][$index] = $col['column'];
             $responseData[$row_position]['positions'][$index] = $col['col_position'];
-            $responseData[$row_position]['componentIds'][$index] = $col['component_id'];
+            
+            if(mysqli_num_rows(mysqli_query($conn,"SELECT DISTINCT vertical_col_position FROM columns WHERE dashboard_id = '$dashboard_id' AND row_position = '$row_position' AND col_position = '".$col['col_position']."';")) > 1){
+                $responseData[$row_position]['componentIds'][$index] = [];
+                $vcolpos = mysqli_query($conn,"SELECT * FROM columns WHERE dashboard_id = '$dashboard_id' AND row_position = '$row_position' AND col_position = '".$col['col_position']."';");
+                while($rvcolpos = mysqli_fetch_assoc($vcolpos)){
+                    $responseData[$row_position]['componentIds'][$index][] = $rvcolpos['component_id'];
+                }
+            }else{
+                $responseData[$row_position]['componentIds'][$index] = $col['component_id'];
+            }
 
             $index++;
         }
