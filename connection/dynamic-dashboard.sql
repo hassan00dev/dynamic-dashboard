24af49e4d71c92cfba5d4789f3c527cf006b978a
codingwithhassan <codingwithme.hassan@gmail.com>
2021-05-22 12:08:52 -0700

    update DB SQL

--- connection/dynamic-dashboard.sql
+++ connection/dynamic-dashboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2021 at 10:03 AM
+-- Generation Time: May 22, 2021 at 09:08 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.3.27
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `drapdrop-adminlte`
+-- Database: `dynamic-dashboard`
 --
 
 -- --------------------------------------------------------
@@ -34,6 +34,15 @@ CREATE TABLE `architectures` (
   `pattern` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `architectures`
+--
+
+INSERT INTO `architectures` (`id`, `dashboard_id`, `row_position`, `pattern`) VALUES
+(195, 1, 0, '12'),
+(196, 1, 1, '6,6'),
+(197, 1, 2, '3,3,3,3');
+
 -- --------------------------------------------------------
 
 --
@@ -46,8 +55,20 @@ CREATE TABLE `columns` (
   `component_id` int(11) DEFAULT NULL,
   `row_position` int(11) NOT NULL,
   `col_position` int(11) NOT NULL DEFAULT 0,
+  `vertical_col_position` int(11) NOT NULL DEFAULT 0,
   `dashboard_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `columns`
+--
+
+INSERT INTO `columns` (`id`, `column`, `component_id`, `row_position`, `col_position`, `vertical_col_position`, `dashboard_id`) VALUES
+(309, 12, 3, 0, 0, 0, 1),
+(310, 6, 4, 1, 0, 0, 1),
+(311, 6, 4, 1, 1, 0, 1),
+(312, 3, 7, 2, 1, 0, 1),
+(313, 3, 8, 2, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -68,7 +89,14 @@ CREATE TABLE `components` (
 INSERT INTO `components` (`id`, `title`, `icon`) VALUES
 (1, 'Sales', 'fas fa-chart-pie'),
 (2, 'Bars', 'fas fa-bars'),
-(3, 'Users', 'fas fa-users');
+(3, 'Users', 'fas fa-users'),
+(4, 'VS Code', 'fas fa-desktop'),
+(5, 'Brackets Editor', 'fas fa-edit'),
+(6, 'To-do', 'fas fa-th-list'),
+(7, 'component 1', 'fas fa-box'),
+(8, 'component 2', 'fas fa-box'),
+(9, 'component 3', 'fas fa-box'),
+(10, 'component 4', 'fas fa-box');
 
 -- --------------------------------------------------------
 
@@ -87,7 +115,29 @@ CREATE TABLE `dashboards` (
 --
 
 INSERT INTO `dashboards` (`id`, `name`, `color`) VALUES
-(1, 'First Dashboard', '#20b94f');
+(1, 'default dashboard', '#20b94f');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` text NOT NULL,
+  `token` text DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `token`, `status`) VALUES
+(1, 'codingwithhassan', 'hassan.javed.bcs20021@gmail.com', '$2y$10$iwICOeTYPI63N/Xn5tGt/eLD0Vl29lrf.Gkl/JaICj7xPNVIfbLLS', NULL, 1);
 
 --
 -- Indexes for dumped tables
@@ -123,6 +173,12 @@ ALTER TABLE `dashboards`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -130,24 +186,30 @@ ALTER TABLE `dashboards`
 -- AUTO_INCREMENT for table `architectures`
 --
 ALTER TABLE `architectures`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=198;
 
 --
 -- AUTO_INCREMENT for table `columns`
 --
 ALTER TABLE `columns`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=137;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=314;
 
 --
 -- AUTO_INCREMENT for table `components`
 --
 ALTER TABLE `components`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `dashboards`
 --
 ALTER TABLE `dashboards`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
