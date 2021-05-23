8deaf1b01a987196d44e3e399aebb13fbdc64b2d
codingwithhassan <codingwithme.hassan@gmail.com>
2021-05-23 23:24:30 +0500

    - to _ for database name

--- connection/connect.php
+++ connection/connect.php
@@ -2,7 +2,7 @@
 define('APP_NAME','DragDrop Application');
 define('APP_URL','http://localhost/dynamic-dashboard');
 
-$conn = mysqli_connect('localhost','root','','dynamic-dashboard');
+$conn = mysqli_connect('localhost','root','','dynamic_dashboard');
 
 if(mysqli_errno($conn) > 0){
     die('database connection not established. '.mysqli_error($conn));
